a6ffa96d97f6e391ed7f5c65f1462e0eba7bea86
Oscar J Abarca Pena <45542113+oscabape10@users.noreply.github.com>
2019-05-16 22:07:00 +0100

    Update Whale Talk.js

--- Whale Talk.js
+++ Whale Talk.js
@@ -4,8 +4,8 @@ const vowels = ['a','e','i','o','u'];
 let resultArray = [];
 //loop to iterate through each letter of the input variable text
 for(let inputIndex = 0; inputIndex < input.length; inputIndex ++){
-// console.log('i is ' + inputIndex);
-//Create a nested for loop inside of the for loop you just wrote.the inner loop iterate through the vowels array each time the outer loop runs.  
+//  test code. console.log('i is ' + inputIndex);
+//A nested for loop inside of the for loop above.the inner loop iterate through the vowels array each time the outer loop runs.  
 for(let vowel = 0; vowel < vowels.length; vowel++ ){
 //code block that compares the input letter to every letter in the vowels array.
   if(input[inputIndex] === vowels[vowel]){ 
@@ -13,7 +13,7 @@ for(let vowel = 0; vowel < vowels.length; vowel++ ){
      if(input[inputIndex] === 'e'){
          resultArray.push('ee')
      }
-//double the letter uRe-create the if statement, but modify it so it pushes the letter u a second time.
+//double the letter, if statement, modify so it pushes the letter 'u' a second time.
       else if(input[inputIndex] === 'u'){
       resultArray.push('uu');  
       }
